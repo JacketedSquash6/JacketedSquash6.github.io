be3dc698275bc90a4c384209e1af2396861aa9b5
JacketedSquash6 <taraspylypenko@gmail.com>
2021-12-16 07:35:31 -0800

    Finalizing Landing Page

--- newYear/js/time.js
+++ newYear/js/time.js
@@ -8,7 +8,7 @@ function refresh() {
     const hourLength = 3600;
     const minuteLength = 60;
 
-    var timeTillNewYear = newYearTime - curTime;
+    var timeTillNewYear = Math.max(newYearTime - curTime, 0);
 
     days = parseInt(timeTillNewYear / dayLength);
     hours = parseInt((timeTillNewYear % dayLength) / hourLength);
@@ -19,4 +19,26 @@ function refresh() {
     document.getElementById("hours").innerHTML = hours;
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
+
+    var remark = "Waitin' for the new year";
+    if (days > 7) {
+        remark = "C'mon it's not even the week of. You're not <i>that</i> sick of this year";
+    }
+    else if (days > 0) {
+        remark = "Come back later this week";
+    }
+    else if (hours > 1) {
+        remark = "Have a happy New Year's Day!";
+    }
+    else if (minutes > 0) {
+        remark = "The anticipation is building!";
+    }
+    else if (seconds > 0) {
+        remark = seconds;
+    }
+    else {
+        remark = "HAPPY NEW YEAR!!!!!"
+    }
+
+    document.getElementById("remark").innerHTML = remark;
 }
