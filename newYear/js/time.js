264e595542e803a8f3142b82cc201cada2a630a2
JacketedSquash6 <taraspylypenko@gmail.com>
2022-02-24 16:47:52 -0800

    new year counts down to 2023

--- newYear/js/time.js
+++ newYear/js/time.js
@@ -2,7 +2,7 @@ refresh();
 window.setInterval('refresh()', 1000);
 function refresh() {
     var curTime = parseInt(Date.now() / 1000);
-    const newYearTime = 1641024000; //I got this from a Date-to-Unix-time converter
+    const newYearTime = 1672560000; //I got this from a Date-to-Unix-time converter
 
     const dayLength = 86400;
     const hourLength = 3600;
